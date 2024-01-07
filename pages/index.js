24bfaf4bb26f4702e9b8e392928d213aef95c83d
ysaito <60546319+yk-saito@users.noreply.github.com>
2024-01-07 11:21:19 +0900

    feat: storing a history of moves

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,9 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+function Board({ xIsNext, squares, onPlay }) {
+  // const [xIsNext, setXIsNext] = useState(true);
+  // const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
     // 既にマスが埋まっている、または勝者が決まっている場合は何もしない
@@ -31,8 +31,9 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    // setSquares(nextSquares);
+    // setXIsNext(!xIsNext);
+    onPlay();
   }
 
   const winner = calculateWinner(squares);
@@ -71,6 +72,28 @@ export default function Board() {
       </main>
     </>
   );
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className='game-info'>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  )
 }
 
 function calculateWinner(squares) {
