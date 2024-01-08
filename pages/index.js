3c3c5b3049a4dc080267b45a37a29ccffebc9975
ysaito <60546319+yk-saito@users.noreply.github.com>
2024-01-08 21:34:18 +0900

    feat: save current move

--- pages/index.js
+++ pages/index.js
@@ -77,6 +77,8 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  // ユーザーが現在見ている盤面の着手番号を保持する
+  const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
@@ -84,13 +86,33 @@ export default function Game() {
     setXIsNext(!xIsNext);
   }
 
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 == 0); // 偶数`X`、奇数`O`
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className='game-info'>
-        <ol>{/* TODO */}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   )
